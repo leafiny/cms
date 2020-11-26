dc56d97d2f2eef4391b9e32395bdb8a6e11993a2
leafiny <mvion@leafiny.com>
2020-11-26 14:11:22 +0100

    Apply spaceless for nocache blocks

--- modules/Leafiny_Fpc/app/Fpc/Helper/Cache.php
+++ modules/Leafiny_Fpc/app/Fpc/Helper/Cache.php
@@ -195,7 +195,7 @@ class Fpc_Helper_Cache extends Core_Helper
             foreach ($matches['blocks'] as $block) {
                 $render = preg_replace(
                     '@<!-- nocache::' . $block . ' -->(.*)<!-- /nocache::' . $block . ' -->@s',
-                    '{% include ' . $block . '|block ignore missing %}',
+                    '{% apply spaceless %}{% include ' . $block . '|block ignore missing %}{% endapply %}',
                     $render
                 );
             }
