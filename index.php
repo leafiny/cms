2b4eb25a05028119ed4a4ec8b9a3800e93060fd2
leafiny <mvion@leafiny.com>
2020-12-09 11:16:38 +0100

    Fix display errors when DEVELOPER_MODE is set to 0

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', '0');
 
-if (isset($_SERVER['DEVELOPER_MODE'])) {
+if (!empty($_SERVER['DEVELOPER_MODE'])) {
     error_reporting(E_ALL | E_STRICT);
     ini_set('display_errors', '1');
 }
