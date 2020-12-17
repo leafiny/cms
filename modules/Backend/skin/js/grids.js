85fe0c0014225c54ccb205ca611561b89e25194e
leafiny <mvion@leafiny.com>
2020-12-17 20:22:50 +0100

    Code moved to dedicated script

--- modules/Backend/skin/js/grids.js
+++ modules/Backend/skin/js/grids.js
@@ -1,2 +1,54 @@
 gridSelectAll();
 gridFilter();
+
+/**
+ * Check or un-check all lines when input in table head is checked
+ *
+ * @returns {boolean}
+ */
+function gridSelectAll () {
+    let select = document.querySelector('.select-all');
+
+    if (!select) {
+        return false;
+    }
+
+    select.addEventListener('click', function () {
+        let inputs = document.querySelectorAll('.select-id');
+        let checked = false;
+        if (select.checked) {
+            checked = true;
+        }
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].checked = checked;
+        }
+    });
+
+    return true;
+}
+
+/**
+ * Auto-select filter option in toolbar when a filter is updated
+ *
+ * @returns {boolean}
+ */
+function gridFilter () {
+    let filters = document.querySelector('.filters');
+    let action = document.getElementById('toolbar-select-action');
+
+    if (!filters || !action) {
+        return false;
+    }
+
+    let selects = filters.querySelectorAll('select, input');
+    for (let i = 0; i < selects.length; i++) {
+        selects[i].addEventListener('change', function () {
+            action.selectedIndex = 0;
+            if (selects[i].tagName === 'SELECT') {
+                document.querySelector('.container-grid').submit();
+            }
+        });
+    }
+
+    return true;
+}
--- modules/Backend/skin/js/users/form.js
+++ modules/Backend/skin/js/users/form.js
@@ -1,1 +1,32 @@
 userResources();
+
+/**
+ * Hide or show user resources in user form
+ *
+ * @returns {boolean}
+ */
+function userResources () {
+    let userIsAdmin = document.getElementById('user-edit-is-admin');
+    let userResources = document.getElementById('user-edit-resources');
+
+    if (!userIsAdmin || !userResources) {
+        return false;
+    }
+
+    let options = userIsAdmin.getElementsByTagName('option');
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].selected && options[i].value === '1') {
+            userResources.style.display = 'none';
+        }
+    }
+
+    userIsAdmin.addEventListener('change', function () {
+        if (this.value === '1') {
+            userResources.style.display = 'none';
+        } else {
+            userResources.style.display = 'block';
+        }
+    });
+
+    return true;
+}
