198eecdbae4d0b2c21c943201fa3c011b33020ee
leafiny <mvion@leafiny.com>
2021-01-07 11:39:20 +0100

    Useless indent removed

--- core/app/Core/Template/Abstract.php
+++ core/app/Core/Template/Abstract.php
@@ -218,7 +218,7 @@ abstract class Core_Template_Abstract extends Leafiny_Object
         if ($key === null) {
             return '';
         }
-        
+
         return App::translate($key) ?: $key;
     }
 
