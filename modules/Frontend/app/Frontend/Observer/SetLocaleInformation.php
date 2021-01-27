18928379ae7baa945b64519b7194c6c201c26d0c
leafiny <mvion@leafiny.com>
2021-01-27 09:20:48 +0100

    Missing file licence added

--- modules/Frontend/app/Frontend/Observer/SetLocaleInformation.php
+++ modules/Frontend/app/Frontend/Observer/SetLocaleInformation.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of Leafiny.
+ *
+ * Copyright (C) Magentix SARL
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 /**
  * Class Frontend_Observer_SetLocaleInformation
