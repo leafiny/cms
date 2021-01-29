4a33beccbda427398941ebb11106da6b09e2b154
leafiny <mvion@leafiny.com>
2021-01-29 15:41:13 +0100

    New Release - 1.6.3

--- core/App.php
+++ core/App.php
@@ -21,7 +21,7 @@ final class App
     /**
      * @var string VERSION
      */
-    public const VERSION = '1.6.2';
+    public const VERSION = '1.6.3';
     /**
      * @var string MODULES_DIRECTORY
      */
