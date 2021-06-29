786087c828ba8a7aa1ec47cc830a3c344057cca4
leafiny <mvion@leafiny.com>
2021-06-29 17:29:33 +0200

    New Release - 1.8.1

--- core/App.php
+++ core/App.php
@@ -21,7 +21,7 @@ final class App
     /**
      * @var string VERSION
      */
-    public const VERSION = '1.8.0';
+    public const VERSION = '1.8.1';
     /**
      * @var string MODULES_DIRECTORY
      */
