d38feeeb47675b0c6c908ddcefa2efeb3ffc0e71
Leafiny <mvion@leafiny.com>
2022-07-04 12:12:45 +0200

    Bump version

--- core/App.php
+++ core/App.php
@@ -21,7 +21,7 @@ final class App
     /**
      * @var string VERSION
      */
-    public const VERSION = '1.10.3';
+    public const VERSION = '1.10.4';
     /**
      * @var string MODULES_DIRECTORY
      */
